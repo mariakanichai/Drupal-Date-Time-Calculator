767cffd0b1f16d343e534e35ae20f46becf7e81f
mjose <mjose@opentrends.net>
2022-04-24 13:33:22 +0530

    added custom block in module and template file integrated

---
+++ web/modules/custom/time_calculation/src/Plugin/Block/TimeBlock.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\time_calculation\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\time_calculation\TimeCalculationService;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'Time displaying' Block.
+ *
+ * @Block(
+ *   id = "time_block",
+ *   admin_label = @Translation("Time block"),
+ * )
+ */
+class TimeBlock extends BlockBase implements ContainerFactoryPluginInterface {
+  protected $timeCalculation;
+
+  /**
+   * Constructor to inject the time calculation service
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, TimeCalculationService $timeCalculation) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->timeCalculation = $timeCalculation;
+  }
+
+  /**
+   *
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('time_calculation.custom_service')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = \Drupal::config('time_calculation.settings');
+    $city = $config->get('city');
+    $details = $this->timeCalculation->myLogic();
+
+    return [
+      '#theme' => 'time_block',
+      '#city' => $details['city'],
+      '#dateTime' => $details['dateTime'],
+      '#country' => $details['country'],
+
+    ];
+  }
+
+}
