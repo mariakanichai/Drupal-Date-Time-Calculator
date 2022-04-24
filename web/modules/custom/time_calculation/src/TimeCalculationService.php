2d9c76d752977b5001c64fbae45acffa38e55e72
mjose <mjose@opentrends.net>
2022-04-24 13:32:09 +0530

    added service file to calculate time based on the timezone

---
+++ web/modules/custom/time_calculation/src/TimeCalculationService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\time_calculation;
+
+use Drupal\Core\Datetime\DrupalDateTime;
+
+/**
+ * Service with config.
+ */
+class TimeCalculationService {
+
+  /**
+   * Drupal's settings manager.
+   */
+  protected $config;
+  protected $date;
+
+  /**
+   * Constructor.
+   */
+  public function __construct() {
+    $this->config = \Drupal::config('time_calculation.settings');
+    $this->date = new DrupalDateTime();
+
+  }
+
+  /**
+   * Calculate time based on the timezone config values.
+   */
+  public function myLogic() {
+    $country = $this->config->get('country');
+    $timeZone = $this->config->get('timezone');
+    $city = $this->config->get('city');
+
+    $this->date->setTimezone(new \DateTimeZone($timeZone));
+    $currentDate = $this->date->format('jS M - Y H:i A');
+
+    $details = [
+      "city" => $city,
+      "country" => $country,
+      "dateTime" => $currentDate,
+    ];
+
+    return $details;
+  }
+
+}
